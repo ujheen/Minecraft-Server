7365babcb489545c91cdfce22102e57d2681f328
BuildTools <unconfigured@null.spigotmc.org>
2021-03-23 07:18:01 +0800

    Test Commit from IntelliJ

---
+++ src/com/ujheen/permissions/permissions.java
@@ -0,0 +1,15 @@
+package com.ujheen.permissions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class permissions extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
